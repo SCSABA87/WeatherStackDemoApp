3856928c2fddc6c8d2578413a9a63a9d177c96a7
Csaba Sarkadi <sarkadicsaba26@gmail.com>
2020-10-01 19:46:35 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const http = require("http");
+
+
+const app = express();
+
+app.set(`view engine`, `ejs`);
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
+
+app.get("/", function(req, res) {
+
+  res.sendFile(__dirname + "/index.html")
+
+});
+
+app.post("/",function(req,res){
+
+
+const query = req.body.city;
+
+console.log(query);
+
+
+const url = "http://api.weatherstack.com/current?access_key=4098fe641cddbf398963874d1852c645&query="+ query + "&units=m"
+
+const request = http.get(url,function(response){
+
+response.on("data",function(data){
+
+const jsondata = JSON.parse(data);
+
+const temperature = jsondata.current.temperature;
+
+const imageURL = jsondata.current.weather_icons[0]
+
+const responseMessage = "The current temperature in " + query + " is: " + temperature + " Celsius degrees"
+
+
+res.render("response", {
+
+  responseMessage: responseMessage,
+  imageURL: imageURL,
+
+});
+
+
+
+res.send();
+
+
+
+
+});
+
+
+});
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server is running on port 3000")
+});
+
+
+
+// Weatherstack api key  4098fe641cddbf398963874d1852c645
